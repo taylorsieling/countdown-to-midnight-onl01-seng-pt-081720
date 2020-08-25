c980149f766d46dade2546b9c9fba55d04a55fe0
Taylor Sieling <taylorasieling@gmail.com>
2020-08-25 01:12:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def countdown_until_sleep(number)
+ while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep(1.second)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+countdown_until_sleep(10)
+  

0213d11cf71db6400adb4b1911cc1a08af06e043
Taylor Sieling <taylorasieling@gmail.com>
2020-08-25 01:17:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,8 +12,8 @@ countdown(10)
 
 def countdown_until_sleep(number)
  while number > 0
-    puts "#{number} SECOND(S)!"
     sleep 1
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
   "HAPPY NEW YEAR!"
